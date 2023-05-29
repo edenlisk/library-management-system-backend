5985758e1bf4d612c8b10072c8026490eaf352d8
edenlisk <nkumbuyenock@gmail.com>
2023-05-29 15:07:53 +0200

    Fix bug in sending error message

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -66,11 +66,10 @@ module.exports = (err, req, res, next) => {
         // console.log(error);
         sendErrorDev(err, res);
     } else if (process.env.NODE_ENV === 'production') {
-        let error = { ...err };
-        if (error.name === 'CastError') error = handleCastErrorDB(error);
-        if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-        if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
-        sendErrorProd(error, res);
+        if (err.name === 'CastError') err = handleCastErrorDB(err);
+        if (err.code === 11000) err = handleDuplicateFieldsDB(err);
+        if (err.name === 'ValidationError') err = handleValidationErrorDB(err);
+        sendErrorProd(err, res);
     }
 
 };
